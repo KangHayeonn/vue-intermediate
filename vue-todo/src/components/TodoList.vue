2817554897769bd782811aa58989fbf2814af95a
KangHayeonn <rkdgkdus522@naver.com>
2023-03-02 17:05:14 +0900

    [ADD]: TodoList 컴포넌트의 할 일 목록 표시 기능 구현

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,10 +1,26 @@
 <template>
-  <div>List</div>
+  <div>
+    <!-- ul>li*3 -->
+    <ul>
+        <li v-for="(todoItem, idx) in todoItems" :key="idx">{{ todoItem }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            todoItems: []
+        }
+    },
+    created: function() {
+        if(localStorage.length > 0) {
+            for(let i=0; i<localStorage.length; i++) {
+                this.todoItems.push(localStorage.key(i))
+            }
+        }
+    }
 }
 </script>
 
