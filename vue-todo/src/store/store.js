d68fe20a38b130006dc6ad8171b66c3332ee6f87
KangHayeonn <rkdgkdus522@naver.com>
2023-03-06 13:56:48 +0900

    [ADD]: vuex 설치 및 등록

---
+++ vue-todo/src/store/store.js
@@ -0,0 +1,8 @@
+import Vue from "vue"
+import Vuex from "vuex"
+
+Vue.use(Vuex) // use : vue의 plugin 기능, vue를 사용할 때 전역으로 특정 기능을 추가하고 싶을 때 사용 (global funtionality 추가) 
+
+export const store = new Vuex.Store({
+    //
+});
